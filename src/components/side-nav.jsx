ca5548018eb1e421b124d8aca361e323d8849772
abisola <abisola.ca@gmail.com>
2021-08-16 17:24:59 +0100

    fixed side nav

--- src/components/side-nav.jsx
+++ src/components/side-nav.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import close from '../images/close.svg'
 
 const SideNav = () => {
     
     const showMenu = () => {
         let menu = document.querySelector(".side-nav")
+        let links = document.querySelectorAll(".links")
         
         if (menu.style.right !== "0px") {
           menu.setAttribute("style", "right: 0;");
@@ -20,11 +22,11 @@ const SideNav = () => {
     return (
         <div className="side-nav">
             <ul className="side-nav-items">
-                <button className="close" onClick={showMenu}><svg width="20" height="20" xmlns="http://www.w3.org/2000/svg"><path d="M17.778.808l1.414 1.414L11.414 10l7.778 7.778-1.414 1.414L10 11.414l-7.778 7.778-1.414-1.414L8.586 10 .808 2.222 2.222.808 10 8.586 17.778.808z" fill="#0000000" fill-rule="evenodd"/></svg></button>
-                    <a href="#home" onClick={closeOnClick}><li>Home</li></a>
-                    <a href="#about" onClick={closeOnClick}><li>About Me</li></a>
-                    <a href="#works" onClick={closeOnClick}><li>My Works</li></a> 
-                    <a href="#contact" onClick={closeOnClick}><li>Contact Me</li></a>
+                <button className="close" onClick={showMenu}><img src={close} /></button>
+                    <a className="links" href="#home" onClick={closeOnClick}><li>Home</li></a>
+                    <a className="links" href="#about" onClick={closeOnClick}><li>About Me</li></a>
+                    <a className="links" href="#works" onClick={closeOnClick}><li>My Works</li></a> 
+                    <a className="links" href="#contact" onClick={closeOnClick}><li>Contact Me</li></a>
                 </ul>
         </div>
     )
