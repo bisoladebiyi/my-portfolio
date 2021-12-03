a48c2704d0c2dd710dfdc77128e9d3ea35bc5e0c
abisola <abisola.ca@gmail.com>
2021-12-03 03:18:51 +0100

    added new project

--- src/works.js
+++ src/works.js
@@ -2,6 +2,7 @@ import loopstudio from './images/loopstudio.png'
 import grocery from './images/grocery.png'
 import trafalgar from './images/trafalgar.png'
 import resume from './images/resume.png'
+import weather from './images/weather.png'
 
 
 
@@ -18,28 +19,38 @@ const works = [
     },
     {
         id: 2,
+        img: weather,
+        desc: "A weather app, gives basic info about the weather of all cities across the globe",
+        addclass: "work switch",
+        title: "Weather App",
+        github: "https://github.com/bisoladebiyi/weather-app",
+        site: "https://bee-weather-app.herokuapp.com/"
+
+    },
+    {
+        id: 3,
         img: grocery,
         desc: "A react web app used for adding/removing items i.e grocery, to/from a  grocery list.",
-        addclass: "work switch",
+        addclass: "work",
         title: "Grocery Bud App",
         github: "https://github.com/bisoladebiyi/react-mini-projects/tree/main/grocery-bud",
         site: "https://grocery-bud-bee.vercel.app/"
 
     },
     {
-        id: 3,
+        id: 4,
         img: trafalgar,
         desc: "A simple landing page, fully responsive. Built with HTML, CSS & Javascript.",
-        addclass: "work",
+        addclass: "work switch",
         title: "Trafalgar Landing Page",
         github: "https://github.com/bisoladebiyi/Trafalgar-landing-page",
         site: "https://trafalgar-bee.netlify.app/"
 
     }, {
-        id:4,
+        id:5,
         img: resume,
         desc: "A single webpage resume. Fully responsive and built with React.js.",
-        addclass: "work switch",
+        addclass: "work",
         title: "My Resume",
         github: "https://github.com/bisoladebiyi/HNGi8-tasks-stage-2/tree/main/resume-hng-task",
         site: "https://bisoladebiyi-resume.vercel.app/"
