1daf37731297f424aa6dbc03131fa446f5e0b848
abisola <abisola.ca@gmail.com>
2021-12-22 03:02:40 +0100

    created new version of portfolio

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,26 +1,15 @@
 import React from 'react'
-import img from '../images/IMG_3445.jpg'
-
+import { Link } from 'react-router-dom'
 
 const Header = () => {
    
     return (
-
-        <div className="header" data-aos="fade-up" id="home">
+        <div className="header" data-aos="fade-right">
             <div className="header-text">
-                <p>Hi there! 👋</p>
-                <p className="name">I'm <span>Adebiyi Abisola Oreoluwa</span></p>
-                <p className="job">A FRONTEND WEB DEVELOPER</p>
-                <a href="#contact" rel="noreferrer" className="btn">HIRE ME</a>
+                <p>Hello, I'm Abisola</p>
+                <p className="job">A FRONT-END <br />DEVELOPER</p>
+                <p>I code cool stuff! <Link to="/works">Check it out.</Link></p>
             </div>
-
-            <div className="image-container">
-            
-                <figure className="img-figure"><img className="image" src={img} alt="" /></figure>
-                
-            </div>
-
-
         </div>
     )
 }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,35 +1,13 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 const Navbar = () => {
+  return (
+    <div className="navbar" data-aos="fade-down">
+      <div className="nav">
+        <Link to="/" className="logo">abisola.</Link>
+      </div>
+    </div>
+  );
+};
 
-    const showMenu = () => {
-        let menu = document.querySelector(".side-nav")
-        if (menu.style.right !== "0px") {
-          menu.setAttribute("style", "right: 0;");
-        }
-         else {
-          menu.setAttribute("style", "right: -300px;");
-        }
-    };
-    
-    return (
-      
-        <div className="navbar" data-aos="fade-down">
-            <div className="nav">
-            <p className="logo">A.</p>
-            <div>
-                <ul className="nav-items">
-                    <a href="#home"><li>Home</li></a>
-                    <a href="#about"><li>About Me</li></a>
-                    <a href="#works"><li>My Works</li></a> 
-                    <a href="#contact" className="btn-contact"><li>Contact Me</li></a>
-                    <button className="hamburger" onClick={showMenu}><svg fill="#6DECB9" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 30 30" width="30px" height="30px"><path d="M 3 7 A 1.0001 1.0001 0 1 0 3 9 L 27 9 A 1.0001 1.0001 0 1 0 27 7 L 3 7 z M 3 14 A 1.0001 1.0001 0 1 0 3 16 L 27 16 A 1.0001 1.0001 0 1 0 27 14 L 3 14 z M 3 21 A 1.0001 1.0001 0 1 0 3 23 L 27 23 A 1.0001 1.0001 0 1 0 27 21 L 3 21 z"/></svg></button>
-                </ul>
-            </div>
-            </div>
-          
-        </div> 
-
-    )
-}
-
-export default Navbar
+export default Navbar;
