7577ef5ac19e0e60bd06144ffd7a2e4344a12f10
Abisola Adebiyi <abisola.ca@gmail.com>
2023-09-11 12:54:42 -0700

    update resume link

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -113,7 +113,7 @@ const About = () => {
             </div>
           </div>
           <a
-            href="https://drive.google.com/file/d/1JYVz9sB0U8W1t5Mnb9vYnyWz6OylEwOg/view?usp=sharing"
+            href="https://drive.google.com/file/d/1Ayz4tCvq60vznd6vsb51DxHCTcAuP2oj/view?usp=sharing"
             target={"_blank"}
             rel="noreferrer"
           >
