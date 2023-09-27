d518b2e96b8efe220f16b02b3cd1aef7002c91d4
Abisola Adebiyi <abisola.ca@gmail.com>
2023-09-27 07:50:54 -0700

    add new work

--- src/works.js
+++ src/works.js
@@ -1,108 +1,111 @@
-import weather from './images/weather.png'
-import code from './images/code.png'
-import blue from './images/blue.png'
-import converter from './images/converter.png'
-import slack from './images/slack.png'
-import share from './images/share.png'
-import employee_sector from './images/employee_sector.png'
-import sneaks from './images/sneaks.png'
-import netflix from './images/netflix.png'
-import solid from './images/solid-ecommerce.png'
-
-
+import weather from "./images/weather.png";
+import code from "./images/code.png";
+import blue from "./images/blue.png";
+import converter from "./images/converter.png";
+import slack from "./images/slack.png";
+import share from "./images/share.png";
+import employee_sector from "./images/employee_sector.png";
+import sneaks from "./images/sneaks.png";
+import netflix from "./images/netflix.png";
+import solid from "./images/solid-ecommerce.png";
+import trello_dupe from "./images/trello_dupe.png";
 
 const works = [
-    {
-        id:14,
-        img:netflix,
-        title: "Netflix Clone",
-        github:"https://github.com/bisoladebiyi/netflix-clone",
-        site:"https://bee-next-netflix-clone.netlify.app/",
-        tech:["Next.js", "Redux toolkit", "Typescript", 'Styled Components']
-    },
-    {
-        id:13,
-        img:sneaks,
-        title: "Sneaks E-commerce site",
-        github:"https://github.com/bisoladebiyi/sneaks",
-        site:"https://sneaks-from-bee.netlify.app/",
-        tech:["React", "Redux toolkit", "Material UI", 'Paystack']
-    },
-    {
-        id:12,
-        img: slack,
-        title: "Slack Clone",
-        github: "https://github.com/bisoladebiyi/slack-clone",
-        site: "https://slack-clone-277f2.web.app/",
-        tech:["React", "Redux", "Firebase", "Styled Components", "Material UI"]
-    },
-    {
-        id:11,
-        img: employee_sector,
-        title: "Employee - Sector",
-        github:"https://github.com/bisoladebiyi/employee-sector-frontend",
-        site:"https://employee-sector-frontend.vercel.app/",
-        tech:["React", "Node.js", "MySQL", "TailwindCSS", "Fullstack"]
-    },
-    {
-        id:10,
-        img: share,
-        title: "Share Social App",
-        github: "https://github.com/bisoladebiyi/share",
-        site: "https://share-one.vercel.app/",
-        tech:["Next.js", "Firebase", "Material UI"]
-    },
-    {
-        id:9,
-        img:solid,
-        title: "Solid E-commerce site",
-        github:"https://github.com/bisoladebiyi/solid-ecommerce",
-        site:"https://solid-ecommerce-site.netlify.app/",
-        tech:["Solid.js", "Typescript", "TailwindCSS"]
-    },
+  {
+    id: 15,
+    img: trello_dupe,
+    title: "Trello Dupe",
+    github: "https://github.com/bisoladebiyi/trello-dupe/",
+    site: "https://trello-dupe.vercel.app/",
+    tech: ["React.js", "Firebase", "Typescript", "TailwindCSS"],
+  },
+  {
+    id: 14,
+    img: netflix,
+    title: "Netflix Clone",
+    github: "https://github.com/bisoladebiyi/netflix-clone",
+    site: "https://bee-next-netflix-clone.netlify.app/",
+    tech: ["Next.js", "Redux toolkit", "Typescript", "Styled Components"],
+  },
+  {
+    id: 13,
+    img: sneaks,
+    title: "Sneaks E-commerce site",
+    github: "https://github.com/bisoladebiyi/sneaks",
+    site: "https://sneaks-from-bee.netlify.app/",
+    tech: ["React", "Redux toolkit", "Material UI", "Paystack"],
+  },
+  {
+    id: 12,
+    img: slack,
+    title: "Slack Clone",
+    github: "https://github.com/bisoladebiyi/slack-clone",
+    site: "https://slack-clone-277f2.web.app/",
+    tech: ["React", "Redux", "Firebase", "Styled Components", "Material UI"],
+  },
+  {
+    id: 11,
+    img: employee_sector,
+    title: "Employee - Sector",
+    github: "https://github.com/bisoladebiyi/employee-sector-frontend",
+    site: "https://employee-sector-frontend.vercel.app/",
+    tech: ["React", "Node.js", "MySQL", "TailwindCSS", "Fullstack"],
+  },
+  {
+    id: 10,
+    img: share,
+    title: "Share Social App",
+    github: "https://github.com/bisoladebiyi/share",
+    site: "https://share-one.vercel.app/",
+    tech: ["Next.js", "Firebase", "Material UI"],
+  },
+  {
+    id: 9,
+    img: solid,
+    title: "Solid E-commerce site",
+    github: "https://github.com/bisoladebiyi/solid-ecommerce",
+    site: "https://solid-ecommerce-site.netlify.app/",
+    tech: ["Solid.js", "Typescript", "TailwindCSS"],
+  },
 
-    {
-        id: 8,
-        img: code,
-        desc: "A code editor for testing and showcasing HTML, CSS and JavaScript code. Built with react and tailwind",
-        addclass: "work switch",
-        title: "Code Editor",
-        github: "https://github.com/bisoladebiyi/code-editor",
-        site: "https://codeditor-react.netlify.app/",
-        tech: ["React", "TailwindCSS"]
-
-    },
-    {
-        id: 7,
-        img: weather,
-        desc: "A weather app, gives basic info about the weather of all cities across the globe",
-        addclass: "work ",
-        title: "Weather App",
-        github: "https://github.com/bisoladebiyi/weather-app",
-        site: "https://bee-weather-app-react.netlify.app/",
-        tech: ["React"]
-
-    },
-    {
-        id: 6,
-        img: blue,
-        desc: "E-commerce site built with react, redux and tailwind",
-        addclass: "work switch",
-        title: "Blue Shop",
-        github: "https://github.com/bisoladebiyi/Blue-shop-website",
-        site: "https://blue-shop.netlify.app/",
-        tech: ["React", "Redux", "TailwindCSS"]
-
-    },
-    {
-        id: 4,
-        img: converter,
-        addclass: "work",
-        title: "Currency Converter",
-        github: "https://github.com/bisoladebiyi/react-currency-converter",
-        site: "https://bee-currency-converter-react.netlify.app/",
-        tech: ["React", "TailwindCSS"]
-    },
-  
-]
-export default works
+  {
+    id: 8,
+    img: code,
+    desc: "A code editor for testing and showcasing HTML, CSS and JavaScript code. Built with react and tailwind",
+    addclass: "work switch",
+    title: "Code Editor",
+    github: "https://github.com/bisoladebiyi/code-editor",
+    site: "https://codeditor-react.netlify.app/",
+    tech: ["React", "TailwindCSS"],
+  },
+  {
+    id: 7,
+    img: weather,
+    desc: "A weather app, gives basic info about the weather of all cities across the globe",
+    addclass: "work ",
+    title: "Weather App",
+    github: "https://github.com/bisoladebiyi/weather-app",
+    site: "https://bee-weather-app-react.netlify.app/",
+    tech: ["React"],
+  },
+  {
+    id: 6,
+    img: blue,
+    desc: "E-commerce site built with react, redux and tailwind",
+    addclass: "work switch",
+    title: "Blue Shop",
+    github: "https://github.com/bisoladebiyi/Blue-shop-website",
+    site: "https://blue-shop.netlify.app/",
+    tech: ["React", "Redux", "TailwindCSS"],
+  },
+  {
+    id: 4,
+    img: converter,
+    addclass: "work",
+    title: "Currency Converter",
+    github: "https://github.com/bisoladebiyi/react-currency-converter",
+    site: "https://bee-currency-converter-react.netlify.app/",
+    tech: ["React", "TailwindCSS"],
+  },
+];
+export default works;
